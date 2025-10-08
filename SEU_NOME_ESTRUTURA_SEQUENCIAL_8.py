e75cefb9f4270df1a2f02e67bf8cfa33676e8e43
ismaelcheremond-ops <ismael_cheremond@estudante.sesisenai.org.br>
2025-10-07 21:04:02 -0300

    u

---
+++ SEU_NOME_ESTRUTURA_SEQUENCIAL_8.py
@@ -0,0 +1,6 @@
+ganho=int(input("quantos ganha por hora:"))
+horas=int(input("horas trabalhadas no mês:"))
+print("ganho por hora",ganho)
+print("horas trabalhadas",horas)
+salario=ganho*horas
+print("seu salarios é de",salario)
